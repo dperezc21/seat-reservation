2abbe4602c649b5a33c9a44d8fcc470f4fe9d431
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-08-04 23:03:03 -0500

    feat: show identifiers in the map

--- src/main/java/org/test/PrintSeatsMap.java
+++ src/main/java/org/test/PrintSeatsMap.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class PrintSeatsMap {
 
+    private static Integer numberRowsCurrent = 1;
+
     public static void print(String[][] matrix) {
         StringBuilder builder = new StringBuilder();
 
@@ -14,13 +16,28 @@ public class PrintSeatsMap {
         System.out.println("X -> Asientos Ocupados");
         System.out.println();
 
+        showColumnLetters(list.size());
         for (String[] strings : list) {
             StringBuilder row = new StringBuilder();
             for (String string : strings) {
                 row.append(string).append(" ");
             }
-            builder.append(row).append("\n");
+            builder.append(concatRowValue(row)).append("\n");
         }
         System.out.println(builder);
+    }
+
+    private static String concatRowValue(StringBuilder row) {
+        row.insert(0, numberRowsCurrent + " ");
+        numberRowsCurrent++;
+        return row.toString();
+    }
+
+    private static void showColumnLetters(int numberColumns) {
+        System.out.print("  ");
+        for (char i = 'a'; i <= ('a' + numberColumns); i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
