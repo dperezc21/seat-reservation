99b26f5930d9c2681cdb0223ca8e4027dee816d4
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-08-05 20:08:05 -0500

    feat: build new seat matrix

--- src/main/java/org/test/SeatMatrixMap.java
+++ src/main/java/org/test/SeatMatrixMap.java
@@ -1,17 +1,31 @@
 package org.test;
 
+import org.test.models.Seat;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class SeatMatrixMap {
 
-    public static String[][] buildSeatMatrix(String value, int numberRows, int numberColumns) {
+    public static String[][] buildInitialSeatMatrix(String value, int numberRows, int numberColumns) {
         String[][] matrix = new String[numberRows][numberColumns];
         List<String[]> list = Arrays.stream(matrix).toList();
 
         for (int i = 0; i < list.size(); i++) {
             for (int j = 0; j < list.get(i).length; j++) {
                 matrix[i][j] = value.toUpperCase();
+            }
+        }
+        return matrix;
+    }
+
+    public static String[][] buildSeatMatrix(Seat[][] seatMatrix) {
+        String[][] matrix = new String[seatMatrix.length][seatMatrix[0].length];
+        List<Seat[]> list = Arrays.stream(seatMatrix).toList();
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).length; j++) {
+                matrix[i][j] = list.get(i)[j].getState().getState();
             }
         }
         return matrix;
