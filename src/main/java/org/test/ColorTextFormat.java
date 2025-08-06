27eb1cf021c6efa574df693354e3c24ad1e4cdb5
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-08-05 20:12:13 -0500

    feat: class to set color format on text

---
+++ src/main/java/org/test/ColorTextFormat.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class ColorTextFormat {
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_RESET = "\u001B[0m";
+
+    public static String redColor(String text) {
+        return ANSI_RED + text + ANSI_RESET;
+    }
+}
