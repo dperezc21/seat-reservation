a15c3c1442dd6e5f9c678d8a70c6676b7898e6b0
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-08-07 11:11:07 -0500

    refactor: use of array list removed

--- src/main/java/org/test/useCases/SeatsList.java
+++ src/main/java/org/test/useCases/SeatsList.java
@@ -3,27 +3,22 @@ package org.test.useCases;
 import org.test.models.Seat;
 import org.test.models.SeatState;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class SeatsList {
 
     private static Seat[][] seats;
 
     public static void buildSeatsReservation(SeatState value, int numberRows, int numberColumns) {
         seats = new Seat[numberRows][numberColumns];
-        List<Seat[]> list = Arrays.stream(seats).toList();
 
-        for (int i = 0; i < list.size(); i++) {
-            List<Seat> seatsRow = new ArrayList<>();
-            for (int j = 0; j < list.get(i).length; j++) {
+        for (int i = 0; i < seats.length; i++) {
+            Seat[] seatsRow = new Seat[numberRows];
+            for (int j = 0; j < seats[i].length; j++) {
                 StringBuilder identifier = new StringBuilder();
                 String numberRow = String.format("%d", i + 1);
                 identifier.append(seatsColumn(j)).append(numberRow);
-                seatsRow.add(new Seat(identifier.toString(), value));
+                seatsRow[j] = new Seat(identifier.toString(), value);
             }
-            seats[i] = Arrays.stream(seatsRow.toArray()).toArray(Seat[]::new);
+            seats[i] = seatsRow;
         }
     }
 
